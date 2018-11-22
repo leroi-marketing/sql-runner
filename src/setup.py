1d7c4fa81335d7a3aa7045ed428b640385c015d7
leroi_dev <jonas.steinmann@leori-consulting.com>
2018-11-22 14:57:02 +0100

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sql-runner',
+    version='v0.1',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='LeROI',
+    author_email='',
+    description='SQL runner for postgres, redshift and snowflake'
+)
