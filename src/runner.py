325efbae5981b5dd0a7fd88a1a4d8dfef4f3315e
Jonas Steinmann <jonas.steinmann@leori-consulting.com>
2019-01-16 11:04:57 +0100

    added one letter options

--- src/runner.py
+++ src/runner.py
@@ -7,10 +7,10 @@ from types import SimpleNamespace
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse arguments SQL runner.')
-    parser.add_argument('--config', nargs='?', default='auth/config.json')
-    parser.add_argument('--execute', nargs='*')
-    parser.add_argument('--test', nargs='*')
-    parser.add_argument('--deps', action='store_const', const=True, default=False)
+    parser.add_argument('-c', '--config', nargs='?', default='auth/config.json')
+    parser.add_argument('-e', '--execute', nargs='*')
+    parser.add_argument('-t', '--test', nargs='*')
+    parser.add_argument('-d', '--deps', action='store_const', const=True, default=False)
     args = parser.parse_args()
 
     with open(args.config) as f:
