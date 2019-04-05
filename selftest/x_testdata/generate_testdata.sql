dcfdebe58d6f39df5a71fb10a6c5225968e9f527
Jonas Steinmann <jonas.steinmann@leroi-consulting.com>
2019-04-05 19:18:20 +0200

    add missing test schema

--- selftest/x_testdata/generate_testdata.sql
+++ selftest/x_testdata/generate_testdata.sql
@@ -1,15 +1,17 @@
 DROP SCHEMA if exists sqlrunner CASCADE;
+DROP SCHEMA if exists d_testdev CASCADE;
 
 CREATE SCHEMA sqlrunner;
+CREATE SCHEMA d_testdev;
 
-CREATE TABLE sqlrunner.x_test_data 
+CREATE TABLE sqlrunner.test_data 
 (
   col1   INT,
   col2   INT,
   col3   INT
 );
 
-INSERT INTO sqlrunner.x_test_data
+INSERT INTO sqlrunner.test_data
 VALUES
 (
   1,
