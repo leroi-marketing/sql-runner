bd959267712b71d17f160414fb5298f1a73b4c91
Alexandru Pisarenco <alexandru.pisarenco@leroi-marketing.com>
2019-08-13 18:56:42 +0200

    Bump version up to 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_long_description():
 
 setup(
     name='sql-runner',
-    version='0.1.0',
+    version='0.2.0',
 
     description="LEROI SQL runner",
     long_description=get_long_description(),
